c5aa0c3dd68fc14ded061ca1d43879859feaa9a9
thataruby <thata@dfi.id>
2025-05-21 12:27:55 +0700

    Experiment 1.2: Understanding how it works.

--- src/main.rs
+++ src/main.rs
@@ -105,6 +105,7 @@ fn main() {
         println!("Thata's Komputer: done!");
     });
 
+    println!("Thata's Komputer: hey hey");
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
     drop(spawner);
